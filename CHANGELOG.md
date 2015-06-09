1d3c57b6d1eae5d1d04482ea47f57d36a6fedfc7
Alex Langberg <alexlangberg@gmail.com>
2015-06-09 22:05:27 +0200

    First commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.0.1 (2015-06-09)
+
+## Features
+
+- First release.
---
+++ test/chronostore.spec.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var chai = require('chai');
+chai.use(require('chai-things'));
+var should = chai.should();
+var goldwasher = require('../lib/chronostore');
