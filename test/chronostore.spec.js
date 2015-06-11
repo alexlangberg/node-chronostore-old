b193def42da6e437ee2cc4cb6dcd67306d32b31f
Alex Langberg <alexlangberg@gmail.com>
2015-06-11 23:09:50 +0200

    Reading function done.

--- test/chronostore.spec.js
+++ test/chronostore.spec.js
@@ -8,7 +8,7 @@ var fs = require('fs-extra');
 var moment = require('moment');
 
 var chronostore = require('../lib/chronostore');
-var folder = './storage';
+var folder = './chronostore';
 var text = 'foo bar';
 var json = {foo: 'bar'};
 
@@ -38,14 +38,14 @@ describe('writing', function() {
       extension: '.csv',
       timestamp: 123,
       uuid: 456,
-      base: './testing',
+      root: './testing',
       format: ['YYYY']
     };
     chronostore.write(text, options, function(error, file) {
       var content = fs.readFileSync(file).toString();
       content.should.equal('foo bar');
-      file.should.equal('./testing/1970/123-456.csv');
-      fs.removeSync(options.base);
+      file.should.equal('./testing/1970/0000000000123-456.csv');
+      fs.removeSync(options.root);
       done();
     });
   });
@@ -73,16 +73,56 @@ describe('writing', function() {
 
 });
 
+describe('reading', function() {
+
+  beforeEach(function(done) {
+    chronostore.write(json, {timestamp: 100}, function() {
+      chronostore.write(json, {timestamp: 2000}, function() {
+        chronostore.write(json, {timestamp: 30000}, function() {
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach(function() {
+    fs.removeSync(folder);
+  });
+
+  it('returns the filenames of target files', function(done) {
+    chronostore.read(150, 5000, function(error, files) {
+      files.length.should.equal(1);
+      parseInt(files[0].name.slice(0,13)).should.equal(2000);
+      done();
+    });
+  });
+
+  it('returns other target files', function(done) {
+    chronostore.read(150, 15000000, function(error, files) {
+      files.length.should.equal(2);
+      parseInt(files[0].name.slice(0,13)).should.equal(2000);
+      parseInt(files[1].name.slice(0,13)).should.equal(30000);
+      done();
+    });
+  });
+});
+
 describe('failing', function() {
 
   it('throws on file write failure', function(done) {
     sinon.stub(chronostore.fs, 'outputFile', function(file, content, callback) {
       return callback('Fake sinon error');
     });
-    chronostore.write(text, function(error, file) {
+    chronostore.write(text, function(error) {
       should.exist(error);
       error.should.equal('Fake sinon error');
-      //fs.removeSync(folder);
+      done();
+    });
+  });
+
+  it('throws on wrong read options', function(done) {
+    chronostore.read(150, 5000, {root: './foo'}, function(error) {
+      should.exist(error);
       done();
     });
   });
